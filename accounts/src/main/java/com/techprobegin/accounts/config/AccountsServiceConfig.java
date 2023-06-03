e07c34ec6c86c70d9f075d3eb49838b882f810fe
Tech Pro Begin <129882847+techprobegin@users.noreply.github.com>
2023-06-03 19:48:43 +0800

    spring-boot-resilience4j-retry-demo example

---
+++ accounts/src/main/java/com/techprobegin/accounts/config/AccountsServiceConfig.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.techprobegin.accounts.config;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author Tech Pro Begin
+ *
+ */
+@Configuration
+@ConfigurationProperties(prefix = "accounts")
+@Getter @Setter @ToString
+public class AccountsServiceConfig {
+
+	 private String msg;
+	 private String buildVersion;
+	 private Map<String, String> mailDetails;
+	 private List<String> activeBranches;
+
+}
